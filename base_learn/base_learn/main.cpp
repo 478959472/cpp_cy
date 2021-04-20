1d6fd4e85af0a8dc7e96188e6cec44dff4d6d0fd
cc <478959472@qq.com>
2021-04-20 21:40:16 +0800

    基本集成日志

--- base_learn/base_learn/main.cpp
+++ base_learn/base_learn/main.cpp
@@ -3,11 +3,17 @@
 
 
 #include <iostream>
+#include "log.h"
+
 
 using namespace std;
 
+sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
+
 int main()
 {
+	cout << "Hello CMake." << endl;
+	SYLAR_LOG_INFO(g_logger) << "mylog";
 	cout << "Hello CMake." << endl;
 	return 0;
 }
